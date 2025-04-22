712118f9aef79c9e6deaaf7f8800abbbb3712019
PaulLyu <polina.lyubeznova@geogracom.com>
2025-04-22 19:25:09 +0300

    feat: добавила запрос для генерации текста с картинки

--- src/hooks/useImageProcessing.ts
+++ src/hooks/useImageProcessing.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 import { useStore } from '@/store/useStore';
 
 export const useImageProcessing = () => {
-  const { imageUrl, isProcessing, setImageUrl, setExtractedText, deleteImage } = useStore();
+  const { imageUrl, isProcessing, setImageUrl, processImage, deleteImage } = useStore();
 
   const uploadImage = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -10,9 +10,7 @@ export const useImageProcessing = () => {
       if (file) {
         const url = URL.createObjectURL(file);
         setImageUrl(url);
-        setExtractedText(
-          'А ещё сторонники тоталитаризма в науке освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, представлены в исключительно положительном свете. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий не даёт нам иного выбора, кроме определения модели развития. Однозначно, представители современных социальных резервов, инициированные исключительно синтетически, рассмотрены исключительно в разрезе маркетинговых и финансовых предпосылок.',
-        );
+        processImage();
       }
     },
     [setImageUrl],
